b63047f82b29bf3b72c9217bb267509616f8fdc9
PlayerThird030 <verevka.rud@gmail.com>
2023-02-12 19:30:45 +0300

    Создал рандомный массив

--- ControlTest/Program.cs
+++ ControlTest/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!2.0");
+Console.WriteLine("");
+string[] originalArray = {"324234", "23", "Pri","gb35","ve","f3j5h","t", ".","wgty4"
+,"fjh4h","Wor","gvh3j","ldr","+_((^","^_^"};
