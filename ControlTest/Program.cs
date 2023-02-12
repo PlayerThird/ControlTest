74728d28737f899863a76420921eea243ec3a953
PlayerThird030 <verevka.rud@gmail.com>
2023-02-12 20:17:13 +0300

    Добавил метод записывающий элементы <3 в новый массив

--- ControlTest/Program.cs
+++ ControlTest/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ControlTest;
+
 Console.WriteLine("Hello, World!2.0");
 Console.WriteLine("");
 string[] originalArray = {"324234", "23", "Pri","gb35","ve","f3j5h","t", ".","wgty4"
 ,"fjh4h","Wor","gvh3j","ldr","+_((^","^_^"};
+ArrayWork.Filter(originalArray);
